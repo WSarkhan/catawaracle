8d409526889886755b9ef4fc74ac9c92dafe0ad9
Ward Sarkhan <143825089+WSarkhan@users.noreply.github.com>
2024-11-13 16:51:31 +0000

    fix: added bottom padding to logo

--- components/atoms/Catawaracle/Catawaracle.tsx
+++ components/atoms/Catawaracle/Catawaracle.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 import { CatFace } from '@/components/foundations';
 
 export const Catawaracle = () => (
-  <View className="flex flex-row pt-5">
+  <View className="flex flex-row py-5">
     <Text className="text-3xl font-bold text-orange-500 ">Cata</Text>
     <View className="transform -rotate-180 ">
       <CatFace color={'rgb(251 146 60)'} />
